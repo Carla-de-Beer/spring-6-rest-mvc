c08a1b8a495ae69d453cfe28526ebf6a2d28e457
Carla de Beer <Carla.de-Beer@adesso.de>
2023-12-21 15:07:25 +0100

    Add ConfigurationPropertiesScan to main executing class

--- src/main/java/dev/cadebe/spring6restmvc/Spring6RestMvcApplication.java
+++ src/main/java/dev/cadebe/spring6restmvc/Spring6RestMvcApplication.java
@@ -2,8 +2,10 @@ package dev.cadebe.spring6restmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Spring6RestMvcApplication {
 
     public static void main(String[] args) {
